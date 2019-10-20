d614fd7b8fd8e1b4be6d0e2e9223eb29f85459b8
Hardeep Singh Narang <hardeepnarang10@gmail.com>
2019-10-20 10:17:12 +0530

    Subclass Responsibility
    Implement class responsibility structure.

--- application.py
+++ application.py
@@ -5,7 +5,85 @@ from functools import partial
 import cv2
 
 
-class Application:
+class Utility:
+
+    pass
+
+    # Implement feedback mechanism
+
+    # Implement warning and flush system
+
+    # Implement attendance export channeling - flush() should trigger export
+
+    # Implement secondary-class frame update: Maybe functionality to show simple text on frame
+
+    # Implement function to check time-status; controller needs to be in-sync with time delta
+
+
+class Monitor(Utility):
+
+    # Start attendance monitor - write authentication check and control here
+    def monitor_cam(self):
+
+        # Setup video capture stream from specified capture device
+        self.capture = cv2.VideoCapture(self.capture_device, cv2.CAP_DSHOW)
+
+        # Change START on btn_monitor to STOP
+        self.btn_monitor.setText(self.qtranslate(self.centralwidget_name, 'STOP'))
+
+        # If self.monitor is True, attendance monitor starts
+        while self.monitor:
+
+            # Capture frames from self.capture source
+            _, frame = self.capture.read()
+
+            # cv2 records frames from camera which are vertically flipped: we need to flip them back
+            frame = cv2.flip(frame, 1)
+
+            # cv2 frames are numpy.ndarray type objects: convert them to QImage
+            qimage = QtGui.QImage(frame, frame.shape[1], frame.shape[0], frame.shape[1] * 3,
+                                  QtGui.QImage.Format_RGB888).rgbSwapped()
+
+            # Further convert QImage to QPixmap object
+            cam_pixmap = QtGui.QPixmap(qimage)
+
+            # Render QPixmap to camera container(QFrame object)
+            self.frame_cam.setPixmap(cam_pixmap)
+
+            # Check if main window is closed: stops attendance monitor if it is
+            if not self.main_window.isVisible():
+                self.stop_monitor()
+
+            # Tell event loop to process events
+            self.application.processEvents()
+
+    # Stop attendance monitor
+    def stop_monitor(self):
+        self.monitor = False
+        self.cam_on = False
+        self.capture.release()
+        # cv2.destroyAllWindows()
+
+        # Clear camera frame - QFrame and reset btn_monitor text
+        self.frame_cam.clear()
+        self.btn_monitor.setText(self.qtranslate(self.centralwidget_name, 'START'))
+
+    # Trigger to start/stop monitor cam
+    def monitor_trigger(self):
+
+        # Check status of attendance monitor to determine further action
+        if self.cam_on:
+            print('Turning off monitor mode...')  # Not the best place to put this message - only for debugging
+            self.stop_monitor()
+
+        else:
+            print('Turning on monitor mode...')  # Again not the best place - Put these messages in start/stop funcs
+            self.monitor = True
+            self.cam_on = True
+            self.monitor_cam()
+
+
+class Application(Monitor):
 
     def __init__(self):
 
@@ -86,66 +164,6 @@ class Application:
 
         # Attach monitor_trigger() as signal to btn_monitor click event
         self.btn_monitor.clicked.connect(partial(self.monitor_trigger))
-
-    # Start attendance monitor - write authentication check and control here
-    def monitor_cam(self):
-
-        # Setup video capture stream from specified capture device
-        self.capture = cv2.VideoCapture(self.capture_device, cv2.CAP_DSHOW)
-
-        # Change START on btn_monitor to STOP
-        self.btn_monitor.setText(self.qtranslate(self.centralwidget_name, 'STOP'))
-
-        # If self.monitor is True, attendance monitor starts
-        while self.monitor:
-
-            # Capture frames from self.capture source
-            _, frame = self.capture.read()
-
-            # cv2 records frames from camera which are vertically flipped: we need to flip them back
-            frame = cv2.flip(frame, 1)
-
-            # cv2 frames are numpy.ndarray type objects: convert them to QImage
-            qimage = QtGui.QImage(frame, frame.shape[1], frame.shape[0], frame.shape[1] * 3,
-                                 QtGui.QImage.Format_RGB888).rgbSwapped()
-
-            # Further convert QImage to QPixmap object
-            cam_pixmap = QtGui.QPixmap(qimage)
-
-            # Render QPixmap to camera container(QFrame object)
-            self.frame_cam.setPixmap(cam_pixmap)
-
-            # Check if main window is closed: stops attendance monitor if it is
-            if not self.main_window.isVisible():
-                self.stop_monitor()
-
-            # Tell event loop to process events
-            self.application.processEvents()
-
-    # Stop attendance monitor
-    def stop_monitor(self):
-        self.monitor = False
-        self.cam_on = False
-        self.capture.release()
-        # cv2.destroyAllWindows()
-
-        # Clear camera frame - QFrame and reset btn_monitor text
-        self.frame_cam.clear()
-        self.btn_monitor.setText(self.qtranslate(self.centralwidget_name, 'START'))
-
-    # Trigger to start/stop monitor cam
-    def monitor_trigger(self):
-
-        # Check status of attendance monitor to determine further action
-        if self.cam_on:
-            print('Turning off monitor mode...')    # Not the best place to put this message - only for debugging
-            self.stop_monitor()
-
-        else:
-            print('Turning on monitor mode...')     # Again not the best place - Put these messages in start/stop funcs
-            self.monitor = True
-            self.cam_on = True
-            self.monitor_cam()
 
 
 if __name__ == '__main__':
